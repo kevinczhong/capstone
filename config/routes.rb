8a4296d7b53134ba7e4eaad23aee95d4e78334b1
Kevin Zhong <kvnzhong@gmail.com>
2022-06-24 13:16:08 -0700

    Generated empty controllers, starting to implement routes.

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  #Property Routes
+  get "/properties", controller: "properties", action: "index"
+  get "/properties/:id", controller: "properties", action: "show"
+  post "/properties", controller: "properties", action: "create"
+  patch "/properties/:id", controller: "properties", action: "create"
+  delete "/properties/:id", controller: "properties", action: "destroy"
 end
