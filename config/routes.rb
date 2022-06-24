f2c0d5c168ad4c06596ec3819075c817c4c4a98c
Kevin Zhong <kvnzhong@gmail.com>
2022-06-24 13:58:55 -0700

    Added provisional routes; note: requires testing.

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,32 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  #Property Routes
+  # User/Session Routes
+  post "/users", controller: "users", action: "create"
+  post "/sessions", controller: "sessions", action: "create"
+
+  # Property Routes
   get "/properties", controller: "properties", action: "index"
   get "/properties/:id", controller: "properties", action: "show"
   post "/properties", controller: "properties", action: "create"
-  patch "/properties/:id", controller: "properties", action: "create"
+  patch "/properties/:id", controller: "properties", action: "update"
   delete "/properties/:id", controller: "properties", action: "destroy"
+
+  # Tour Routes
+  get "/tours", controller: "tours", action: "index"
+  get "/tours/:id", controller: "tours", action: "show"
+  post "/tours", controller: "tours", action: "create"
+  delete "/tours/:id", controller: "tours", action: "destroy"
+
+  # Favorite Routes
+  get "/favorites", controller: "favorites", action: "index"
+  post "/favorites", controller: "favorites", action: "create"
+  delete "/favorites", controller: "favorites", action: "destroy"
+
+  # Image Routes
+  get "/images", controller: "images", action: "index"
+  get "/images/:id", controller: "images", action: "show"
+  post "/images", controller: "images", action: "create"
+  patch "/images/:id", controller: "images", action: "update"
+  delete "/images/:id", controller: "images", action: "destroy"
 end
