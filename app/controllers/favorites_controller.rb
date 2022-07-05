b413e2f66804cfb9b94914fa3bcd7d40a76fda0f
Kevin Zhong <kvnzhong@gmail.com>
2022-07-05 09:38:26 -0700

    Added methods for favorites.

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,19 @@
 class FavoritesController < ApplicationController
+  def index
+    favorites = Favorite.all
+    render json: favorites.as_json
+  end
+
+  def create
+    favorite = Favorite.create(
+      user_id: params[:user_id],
+      property_id: params[:property_id],
+    )
+    favorite.save
+  end
+
+  def destroy
+    favorite = Favorite.find_by(id: params[:id])
+    favorite.destroy
+  end
 end
